dc1bbfb322e91c439a3fbd596fa453d51d62c55b
Diogo Henrique <diogomike138@gmail.com>
2022-05-21 14:17:38 -0300

    Inicializado projeto

---
+++ src/js/script.js
@@ -0,0 +1,43 @@
+let menuItems = document.querySelectorAll('.nav-link');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+
+function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const to = document.querySelector(id).offsetTop;
+
+    window.scroll({
+        top: to - 20,
+        behavior: "smooth",
+    });
+}
+
+btnUpToTop = document.getElementById("btn-up");
+
+// Quando o usuário scrollar mais de 30px do topo da página o botão aparece
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        btnUpToTop.style.display = "block";
+    } else {
+        btnUpToTop.style.display = "none";
+    }
+}
+
+btnUpToTop.addEventListener('click', topFunction);
+
+// Quando o usuário clicar no botão, scroll para o topo da página
+function topFunction() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
