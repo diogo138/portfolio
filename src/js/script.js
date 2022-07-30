83cf00bebf5e0b0ee8f0707ec715ce85f92c0344
Diogo Henrique <diogomike138@gmail.com>
2022-07-30 18:23:56 -0300

    style: add efeito ao scroll.

--- src/js/script.js
+++ src/js/script.js
@@ -41,3 +41,23 @@ function topFunction() {
         behavior: "smooth"
     })
 }
+
+function initAnimaScroll() {
+
+    const sections = document.querySelectorAll('.js-scroll');
+    if(sections.length) {
+        const halfWindow = window.innerHeight * 0.4;
+        function animaScroll() {        
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;            
+                const isSectionVisible = (sectionTop - halfWindow) < 0;
+                if( isSectionVisible ) {
+                    section.classList.add('ativo');
+                }
+            })
+        }
+    }
+    window.addEventListener('scroll', animaScroll);
+}
+
+initAnimaScroll();
